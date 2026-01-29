d0b3b725705e690483a83971d4ade413f367b168
olamet <olamet369@gmail.com>
2026-01-29 04:05:09 +0200

    Update app.pyتعديل للداتا

--- app.py
+++ app.py
@@ -244,7 +244,8 @@ if page == "الرئيسية":
     with col1:
         position = st.selectbox("Visible Road Position", ["L", "C", "R"])
         road = st.selectbox("Visible Road Type", list(speed_data.keys())[1:])
-    with col2:        car1 = st.selectbox("Car 1", speed_data["Vehicle"])
+    with col2:
+        car1 = st.selectbox("Car 1", speed_data["Vehicle"])
         car2 = st.selectbox("Car 2", speed_data["Vehicle"])
         car3 = st.selectbox("Car 3", speed_data["Vehicle"])
     
